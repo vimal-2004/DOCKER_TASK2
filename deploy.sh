6852f037045d1b49cd9f9bab6ffa741c68e326e6
vimal-2004 <148618715+vimal-2004@users.noreply.github.com>
2025-02-05 09:42:57 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+    echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
+    docker tag test ar8888/ar
+    docker push ar8888/ar
+    
